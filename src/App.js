df56d4dd9100c5db568ce3d12dd2f53afbe52579
Kalana Mihiranga <kalanamihiranga9@hotmail.com>
2022-11-12 00:10:54 +0530

    completed header section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello World</h1>
+      {/* Header */}
+      <Header />
+
+      {/* App Body */}
+      <div className='app__body'>
+        
+      </div>
     </div>
   );
 }
